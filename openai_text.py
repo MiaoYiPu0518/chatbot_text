1bfbab0c8bd31f94dc829b3e52d6c3a29a657d99
Cao <YeBilly.Cao@ontario.ca>
2023-05-08 16:13:57 -0400

    code clean up

--- openai_text.py
+++ openai_text.py
@@ -3,41 +3,41 @@ import openai
 from termcolor import colored, cprint
 
 openai.api_key = "sk-" # Input OpenAI API key here
-openai.proxy = ""
+openai.proxy = "" 
 GPT4 = "gpt-4"
-messages = []
+messages = [] # chat history 
 
-sys_print = lambda x : cprint(x, "yellow")
+sys_print = lambda x : cprint(x, "yellow") # Used to print system message
 
-# Main function to start the AI chat
 def main():
     sys_print("Welcome to the AI Chat!")
     ai_chat()
 
 def ai_chat():
     while True:
-        user_input = input("Please enter something: ")  # Ask for user input
-        # Check if the user wants to quit the program
+        user_input = input("Please enter message: ")  # Ask for user input
+        # if enter "quit" then quit the program
         if user_input.lower() == "quit":
             sys_print("Goodbye!\n")
             return
         
         messages.append({"role": "user", "content": user_input})
-        # completion = openai.ChatCompletion.create(model=GPT4, messages=messages, stream=True)
         response = stream_response(openai.ChatCompletion.create(model=GPT4, messages=messages, stream=True))
         messages.append({"role": "assistant", "content": response})
 
 def stream_response(completion):
     full_response = ""
     for chunk in completion:
-        if not hasattr(chunk.choices[0].delta, "content"): continue 
+        # Skip the chunk if it doesn't have "content" attribute
+        if not hasattr(chunk.choices[0].delta, "content"): continue
+        
         content = chunk.choices[0].delta.content
         sys.stdout.write(colored(content, "green"))
         full_response += content
         sys.stdout.flush()
-    print()
+    
+    print() # formatting 
     return full_response
 
-# Call the main function
 if __name__ == "__main__":
     main()
